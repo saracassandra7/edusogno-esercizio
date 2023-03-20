3f58ffc069737a7bcf51787c29964768843d4553
saracassandra7 <saracassandra7@gmail.com>
2023-03-20 15:28:49 +0100

    [add] validation

--- login.php
+++ login.php
@@ -1,9 +1,34 @@
 <?php
   session_start();
+  include "db_conn.php";
+
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Verifico se il token CSRF inviato dal form corrisponde a quello salvato nella sessione
     if (!hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'])) {
       die("CSRF validation failed");
     }
-    // gestire l'invio del form
+
+    // Validazione dati
+    if(isset($_POST['username']) && isset($_POST['password'])){
+      function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    }
+
+    $username = validate($_POST['uname']);
+    $password = validate($_POST['password']);
+
+    if(empty($username)){
+      header("Location: index.php?error=Username is required");
+      exit();
+    }
+    else if(empty($password)) {
+      header("Location: index.php?error=Password is required");
+      exit();
+    }
   }
 ?>
